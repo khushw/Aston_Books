f7aba6f8a481ed4968ad06d7240696e75a214eb4
khushw <waliak@aston.ac.uk>
2019-12-04 01:09:29 +0000

    created product MMC,updated user table,index file updated and route for product

---
+++ database/migrations/2019_12_04_001436_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->double('price');
+            $table->text('description')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->string('author')->nullable();
+            $table->string('book_publisher')->nullable();
+            $table->date('published_date')->nullable();
+            $table->double('quantity')->nullable();
+            $table->double('weight')->nullable();
+            $table->double('pages')->nullable(); 
+            $table->unsignedInteger('user_id')->nullable();           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 @section('content')
-        <h1>{{$title}}</h1>
-        <p>This is a dynamic and friednly website for buying and selling books</p>
+        <div class="jumbotron text-center">
+                <h1>{{$title}}</h1>
+                <p>This is a dynamic and friendly website for buying and selling books</p>
+                <p><a class="btn btn-primary btn-lg" href="login" role="button">Login</a> <a class="btn btn-primary btn-lg" href="register" role="button">Register</a></p>
+        </div>       
 @endsection
